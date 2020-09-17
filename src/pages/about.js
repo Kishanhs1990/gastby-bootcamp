a40f339c000f9870ec976346dc7da4440ded6b9d
Kishan <kishanhs1990@gmail.com>
2020-09-17 15:00:48 +0530

    add routes about, blog and contact to repo

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        Kishan has 4+ years of experience in working on projects of varying
+        scale. He is an energetic software developer and has experience in
+        developing robust code for high-volume businesses. He is a
+        self-directed, enthusiastic worker with a passionate commitment to the
+        software field and learning experience.
+      </p>
+    </div>
+  )
+}
+
+export default About
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import { Link } from 'gatsby';
 
-export default function Home() {
-  return <div>Hello world!</div>
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h1>I'm Kishan, a Software Engineer </h1>
+      <h2>Need a developer ? <Link to="/contact">Contact me.</Link> </h2>
+    </div>
+  )
 }
+
+export default HomePage
